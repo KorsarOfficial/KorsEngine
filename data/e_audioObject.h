f467c8faa0b93482681d8a6f9d767c795d503661
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2023-10-06 16:11:21 +0400

    Update e_audioObject.h

--- data/e_audioObject.h
+++ data/e_audioObject.h
@@ -2,3 +2,13 @@
 #define E_AUDIOOBJECT_H
 
 #include <engine_includes.h>
+typedef uint64_t EHandler;
+typedef struct{
+    void* object;
+    void** sounds;
+    void* g_delayNode;
+    uint32_t num_sounds;
+    float volume;
+    float pitch;
+    float pan;
+}
