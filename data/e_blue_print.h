66f39bb92879a68a567e1fb49699aa3a2f3c638b
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2023-10-06 16:25:29 +0400

    Update e_blue_print.h

--- data/e_blue_print.h
+++ data/e_blue_print.h
@@ -1,2 +1,9 @@
 #ifndef E_BLUE_PRINT_H
 #define E_BLUE_PRINT_H
+
+#include "engine_includes.h"
+#include "e_pipeline_variables.h"
+typedef enum{
+    ENGINE_BLUE_PRINT_FLAG_SINLGE_IMAGE = 0x1,
+    ENGINE_BLUE_PRINT_ARRAY_IMAGE = 0x2,
+}
