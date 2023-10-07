82672a8b425f1fdfe9569aae5914e3815e228f14
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2023-10-07 20:33:13 +0400

    Update e_buffer.h

--- data/e_buffer.h
+++ data/e_buffer.h
@@ -6,3 +6,13 @@
 #include "e_blue_print.h"
 
 #include "e_tools.h"
+
+#define MAX_VERTEX_COUNT 65536
+#define MAX_INDEX_COUNT MAX_VERTEX_COUNT * 3
+
+typedef enum{
+    ENGINE_BUFFER_ALLOCATE_VERTEX,
+    ENGINE_BUFFER_ALLOCATE_INDEX,
+    ENGINE_BUFFER_ALLOCATE_UNIFORM,
+    ENGINE_BUFFER_ALLOCATE_STAGING,
+} BufferAllocType;
