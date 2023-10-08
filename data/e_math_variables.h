c06a15dea6c7cf8a7ea6794c196080be743bdf79
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2023-10-08 15:42:58 +0400

    Update e_math_variables.h

---
+++ data/e_math_variables.h
@@ -0,0 +1,25 @@
+#ifndef E_MATH_VARIABLES_H
+#define E_MATH_VARIABLES_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef struct{
+    float x;
+    float y;
+} vec2;
+
+typedef struct{
+    float x;
+    float y;
+    float z;
+} vec3;
+
+typedef struct{
+    
+}
+}
