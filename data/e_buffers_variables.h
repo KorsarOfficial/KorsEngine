d73a2b25945430e12c2c9b61f19cc62484c887b5
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2023-10-08 15:43:00 +0400

    Update e_buffers_variables.h

--- data/e_buffers_variables.h
+++ data/e_buffers_variables.h
@@ -2,3 +2,12 @@
 #define E_BUFFERS_VARIABLES_H
 
 #include "e_math_variables.h"
+
+#define MAX_BONES 128
+#define MAX_LIGHTS 16
+
+typedef struct {
+    vec2 position;
+    vec2 rotation;
+    vec2 scale;
+} TransformBuffer2D;
