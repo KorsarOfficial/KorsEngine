8bd862cebe1a374dab9a64e305824331c049585f
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2023-10-14 14:46:07 +0400

    Update e_buffers_variables.h

--- data/e_buffers_variables.h
+++ data/e_buffers_variables.h
@@ -22,3 +22,20 @@ typedef struct{
     mat4 view;
     mat4 proj;
 } ModelBuffer3D;
+
+typedef struct {
+    mat4 view;
+    vec3 obj_pos __attribute__ ((aligned (16)));
+    vec3 obj_rot __attribute__ ((aligned (16)));
+    vec3 cam_pos __attribute__ ((aligned (16)));
+    vec3 cam_rot __attribute__ ((aligned (16)));
+} SDFBuffer;
+
+typedef struct{
+    vec4 lightpos;
+    vec4 frustumPlanes[6];
+    float displacementFactor;
+    float tessellationFactor;
+    vec2 viewportDim;
+    float tessellatedEdgeSize;
+} TesselationBuffer;
