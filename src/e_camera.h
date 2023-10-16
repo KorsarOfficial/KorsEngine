53a51f8c06150e79e5a9dde89bdd6a00a8383cb4
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2023-10-16 19:58:44 +0300

    Update e_camera.h

--- src/e_camera.h
+++ src/e_camera.h
@@ -1,2 +1,26 @@
 #ifndef E_CAMERA_H
 #define E_CAMERA_H
+
+#include "engine_includes.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+typedef struct{
+    vec2 position;
+    vec2 rotation;
+    vec2 scale;
+} Camera2D
+
+typedef struct{
+    vec3 position __attribute__ ((aligned (16)));
+    vec3 rotation __attribute__ ((aligned (16)));
+    vec3 scale __attribute__ ((aligned (16)));
+    double view_distance;
+    double view_angle;
+    double view_near;
+} Camera3D;
+
+void Camera2DInit(Camera2D *cam);
+}
