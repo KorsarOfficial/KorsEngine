0c59eb643078e280bab69885d1d96af8616b9e44
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2023-10-26 13:01:39 +0400

    Update e_debuger.h
    EDUMCIEXT ex

--- src/e_debuger.h
+++ src/e_debuger.h
@@ -3,5 +3,11 @@
 #include "engine_includes.h" // using
 
 typedef struct EdDebugUtilsMessengerCreateInfoEXT {
-
-}
+    uint32_t                                sType;
+    const void*                             pNext;
+    uint32_t                                flags;
+    uint32_t                                messageSeverity;
+    uint32_t                                messageType;
+    void*                                   pfnUserCallback;
+    void*                                   pUserData;
+} EdDebugUtilsMessengerCreateInfoEXT;
