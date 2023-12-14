de243af53b3d50ffd7622878a9aae71f4996c455
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2023-12-15 00:37:37 +0400

    xyz (math)

--- includes/e_math.h
+++ includes/e_math.h
@@ -16,4 +16,12 @@ extern "C"
 extern vec3 e_vec3_origin;
 
 extern mat4 edenMat;
+
+float lerp(float a, float b, float t);
+float clamp(float a, float d1, float d2);
+int get_sig(float val);
+
+mat3 rotateX(float theta); // Y axis
+mat3 rotateY(float theta); // Z axis
+mat3 rotateZ(float theta);
 }
