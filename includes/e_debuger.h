73b203aad328b7d52e2027219236de895ccaa0b0
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2023-12-15 00:30:43 +0400

    Update e_debuger.h

--- includes/e_debuger.h
+++ includes/e_debuger.h
@@ -3,13 +3,13 @@
 #include "engine_includes.h" // using
 
 typedef struct EdDebugUtilsMessengerCreateInfoEXT {
-    uint32_t    sType;
-    const void* pNext;
-    uint32_t    flags;
-    uint32_t    messageSeverity;
-    uint32_t    messageType;
-    void*   pfnUserCallback;
-    void*   pUserData;
+    uint32_t            sType;
+    const void*         pNext;
+    uint32_t            flags;
+    uint32_t            messageSeverity;
+    uint32_t            messageType;
+    void*               fnUserCallback;
+    void*               pUserData;
 } EdDebugUtilsMessengerCreateInfoEXT;
 
 typedef struct EdAllocationCallbacks {
