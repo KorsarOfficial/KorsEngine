c93ecfb0cf6f90a5610231bdec02f6f5029c8927
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2023-12-15 00:25:23 +0400

    e_b_v
    fix

--- includes/e_buffers_variables.h
+++ includes/e_buffers_variables.h
@@ -32,7 +32,7 @@ typedef struct {
 } SDFBuffer;
 
 typedef struct{
-    vec4 lightpos;
+    vec4 lightPos;
     vec4 frustumPlanes[6];
     float displacementFactor;
     float tessellationFactor;
@@ -52,14 +52,14 @@ typedef struct {
     vec3 specular __attribute__ ((aligned (16)));
 } PointLight;
 
-typedef struct{
+typedef struct {
     vec3 position __attribute__ ((aligned (16)));
-    vec3 direction __attribute__ ((aligned (16)));
+    vec3  direction __attribute__ ((aligned (16)));
 
     float constant;
     float linear;
     float quadratic;
-    float cutOff
+    float cutOff;
 
     vec3 ambient __attribute__ ((aligned (16)));
     vec3 diffuse __attribute__ ((aligned (16)));
@@ -73,7 +73,7 @@ typedef struct{
     vec3 ambient __attribute__ ((aligned (16)));
     vec3 diffuse __attribute__ ((aligned (16)));
     vec3 specular __attribute__ ((aligned (16)));
-} DirLight
+} DirLight;
 
 typedef struct{
     mat4 proj;
@@ -87,30 +87,30 @@ typedef struct{
 
 typedef struct{
     DirLight dir[MAX_LIGHTS];
-    LightMatrix mats[MAX_LIGHT];
+    LightMatrix mats[MAX_LIGHTS];
     vec4 cascadeSplits __attribute__ ((aligned (16)));
 } DirLightBuffer;
 
 typedef struct{
     PointLight points[MAX_LIGHTS];
     LightPosBuff pos[MAX_LIGHTS];
-} PointLightBuffer
+} PointLightBuffer;
 
 typedef struct{
     SpotLight spots[MAX_LIGHTS];
     LightMatrix mats[MAX_LIGHTS];
 } SpotLightBuffer;
 
-typedef strut{
+typedef struct{
     int num_dirs;
     int num_points;
     int num_spots;
-    int IsEnable;
+    int isEnable;
 } LightStatusBuffer;
 
 typedef struct{
     mat4 mats[MAX_BONES] __attribute__ ((aligned (16)));
     float size __attribute__ ((aligned (16)));
-} InvMatrixsBuffer
+} InvMatrixsBuffer;
 
-#endif
+#endif // E_BUFFERS_VARIABLES_H
