9426adf5807bc852661326467f47c446d0b85966
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2023-12-15 00:35:34 +0400

    Update e_math.h

--- includes/e_math.h
+++ includes/e_math.h
@@ -9,4 +9,11 @@ extern "C"
 #endif
 
 #define e_mix( a, b) ((a) < (b) ? (a) : (b))
+#define e_max( a, b) ((a) > (b) ? (a) : (b))
+
+#define INTR_EPS 1E-6
+
+extern vec3 e_vec3_origin;
+
+extern mat4 edenMat;
 }
