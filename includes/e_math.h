b62680e659e3a408ae39477ee0ec05caf73f2352
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2024-01-09 02:40:00 +0400

    fix

--- includes/e_math.h
+++ includes/e_math.h
@@ -1,5 +1,5 @@
 #ifndef E_MATH_H
-#define E_MATH_H // use
+#define E_MATH_H
 
 #include "engine_includes.h"
 
@@ -8,8 +8,8 @@ extern "C"
 {
 #endif
 
-#define e_mix( a, b) ((a) < (b) ? (a) : (b))
-#define e_max( a, b) ((a) > (b) ? (a) : (b))
+#define e_min( a, b ) ((a) < (b) ? (a) : (b))
+#define e_max( a, b ) ((a) > (b) ? (a) : (b))
 
 #define INTR_EPS 1E-6
 
@@ -21,8 +21,10 @@ float lerp(float a, float b, float t);
 float clamp(float a, float d1, float d2);
 int get_sig(float val);
 
-mat3 rotateX(float theta); // Y axis
-mat3 rotateY(float theta); // Z axis
+mat3 rotateX(float theta);
+// Rotation matrix around the Y axis.
+mat3 rotateY(float theta);
+// Rotation matrix around the Z axis.
 mat3 rotateZ(float theta);
 
 mat3 m3_scale_matrix(vec2 scale );
@@ -35,61 +37,63 @@ vec2 vec2_f(float x, float y);
 
 float v2_cross(vec2 a, vec2 b);
 vec2 v2_add(vec2 a, vec2 b);
-vec2 v2_adds    (vec2 a, float s);
-vec2 v2_sub     (vec2 a, vec2 b);
-vec2 v2_subs    (vec2 a, float s);
-vec2 v2_mul     (vec2 a, vec2 b);
-vec2 v2_muls    (vec2 a, float s);
-vec2 v2_div     (vec2 a, vec2 b);
-vec2 v2_divs    (vec2 a, float s);
+vec2 v2_adds  (vec2 a, float s);
+vec2 v2_sub   (vec2 a, vec2 b);
+vec2 v2_subs  (vec2 a, float s);
+vec2 v2_mul   (vec2 a, vec2 b);
+vec2 v2_muls  (vec2 a, float s);
+vec2 v2_div   (vec2 a, vec2 b);
+vec2 v2_divs  (vec2 a, float s);
 vec2 v2_norm(vec2 v);
 float v2_length(vec2 v);
-float v2_dot    (vec2 a, vec2 b);
-float v2_distance(vec2 v1, vec2 v2);
+float v2_dot (vec2 a, vec2 b);
+float  v2_distance(vec2 v1, vec2 v2);
 
 vec3 vec3_f(float x, float y, float z);
 
 float v3_maxs(vec3 a);
 float v3_mins(vec3 a);
-vec3 v3_abs(vec3, a);
+vec3 v3_abs(vec3 a);
 
 vec3 v3_min(vec3 a, vec3 b);
 vec3 v3_max(vec3 a, vec3 b);
-vec v3_add  (vec3 a, vec3 b);
-vec3 v3_adds    (vec3 a, float s);
-vec3 v3_sub     (vec3 a, vec3 b);
-vec3 v3_subs    (vec3a, vec3 s);
-vec3 v3_mul     (vec3 a, vec3 b);
-vec3 v3_muls    (vec3 a, float s);
-vec3 v3_div     (vec3 a, vec3 b);
-vec3 v3_divs    (vec3 a, float s);
-float v3_distance(vec3 v1, vec3 v2);
-float v3_length(vec3 v);
-vec3 v3_norm (vec3, v);
-float v3_dot    (vec3 a, vec3 b);
-vec3 v3_proj    (vec3 v, vec3 onto);
-vec3 v3_cross   (vec3 a, vec3 b);
-float v3_angle_between(vec3 a, vec3 b);
-bool v3_equal(vec3 a, vec3 vec3 b);
-vec3 v3_lerp(vec3, vec3 b, float t);
+vec3 v3_add   (vec3 a, vec3 b);
+vec3 v3_adds  (vec3 a, float s);
+vec3 v3_sub   (vec3 a, vec3 b);
+vec3 v3_subs  (vec3 a, float s);
+vec3 v3_mul   (vec3 a, vec3 b);
+vec3 v3_muls  (vec3 a, float s);
+vec3 v3_div   (vec3 a, vec3 b);
+vec3 v3_divs  (vec3 a, float s);
+float  v3_distance(vec3 v1, vec3 v2);
+float  v3_length(vec3 v);
+vec3 v3_norm  (vec3 v);
+float  v3_dot   (vec3 a, vec3 b);
+vec3 v3_proj  (vec3 v, vec3 onto);
+vec3 v3_cross (vec3 a, vec3 b);
+float  v3_angle_between(vec3 a, vec3 b);
+bool v3_equal(vec3 a, vec3 b);
+vec3 v3_lerp(vec3 a, vec3 b, float t);
 vec3 v3_slerp(vec3 start, vec3 end, float percent);
 vec3 v3_to(vec3 from, vec3 to, float t);
-float v3_point_segment_dist(const vec3 *P, const vec3 *x0, const vec3 *B, const vec *C, vec3 *witness);
+float v3_point_segment_dist(const vec3 *P, const vec3 *x0, const vec3 *b, vec3 *witness);
 float v3_point_tri_dist(const vec3 *P, const vec3 *x0, const vec3 *B, const vec3 *C, vec3 *witness);
 
 vec4 vec4_f(float x, float y, float z, float w);
 
-vec4 v4_add (vec4 a, vec4 b);
-vec4 v4_sub(vec4 a, vec4 b);
-vec4 v4_mul (vec4 a, vec4 b);
-vec4 v4_mad(vec4 a, vec4 b, float c);
-vec v4_div(vec4 a, float b);
-vec4 v4_neg(vec4 a);
-vec4 v4_inverse_normalized(vec4 a);
+vec4  v4_add(vec4 a, vec4 b) ;
+vec4  v4_sub(vec4 a, vec4 b) ;
+vec4  v4_mul (vec4 a, vec4 b);
+vec4  v4_muls (vec4 a, float s);
+vec4  v4_mad(vec4 a, vec4 b, float c);
+vec4  v4_div(vec4 a, float b);
+vec4  v4_neg(vec4 a) ;
+vec4  v4_inverse(vec4 a) ;
+vec4  v4_inverse_normalized(vec4 a);
 float v4_dot(vec4 a, vec4 b);
 float v4_length(vec4 a);
-vec4 v4_normalize(vec4 a);
-bool v4_equal(vec4 a, vec4 b);
+vec4  v4_normalize(vec4 a);
+bool v4_equal(vec4 a, vec4 b) ;
 vec4 v4_lerp(vec4 a, vec4 b, float t);
 
 mat3 mat3_f();
@@ -98,7 +102,7 @@ vec3 m4_v3_mult(mat4 m, vec3 v);
 vec4 m4_v4_mult(mat4 mat, vec4 v);
 
 mat4 m4_rotate(mat4 mat, float angle, vec3 axis);
-mat4 m4_rotated(mat4 mat, float angle, vec3 axis);
+mat4 m4_rotated(mat4 mat, float angle, vec3 axis) ;
 mat4 m4_rotate_make(mat4 mat, float angle, vec3 axis);
 mat4 m4_mul_rot(mat4 mat1, mat4 mat2) ;
 mat4 mat4_mult_transform(mat4 m1, mat4 m2);
@@ -128,4 +132,4 @@ mat4 m4_inv(mat4 mat);
 }
 #endif
 
-#endif
+#endif // E_MATH_H
