eb016d4bebe6bbe263791e64937d88f26482e0a8
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2024-01-09 02:45:48 +0400

    fix

--- includes/e_painter.h
+++ includes/e_painter.h
@@ -1,2 +1,34 @@
 #ifndef E_PAINTER_H
 #define E_PAINTER_H
+
+#include "gameObject2D.h"
+
+#define MAX_GUI_PAINT_OBJECTS 256
+
+typedef enum{
+    ENGIME_PAINTER_TYPE_BOX,
+    ENGINE_PAINTER_TYPE_CIRCLE,
+    ENGINE_PAINTER_TYPE_ORIENTED_BOX,
+    ENGINE_PAINTER_TYPE_SEGMENT,
+} PAINTER_TYPE_OBJECT;
+
+typedef struct{
+    vec2 position;
+    vec2 size;
+    int type;
+    float angle;
+    float radius;
+    float transparent;
+    vec4 color __attribute__ ((aligned (16)));
+} PObjectParams;
+
+typedef struct{
+    vec2 offset;
+    vec2 position;
+    vec2 size;
+} PainterBuffer;
+
+typedef struct{
+    PObjectParams objs[MAX_GUI_PAINT_OBJECTS];
+    int size;
+}DrawObjectsBuffer;
