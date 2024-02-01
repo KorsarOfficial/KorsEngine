ce46cd5e04f3c60fdbc6cf103e0aef29a635b159
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2024-02-01 17:30:08 +0400

    EPV

--- includes/e_pipeline_variables.h
+++ includes/e_pipeline_variables.h
@@ -4,3 +4,17 @@
 #include <stdint.h>
 
 #include "engine_includes.h"
+
+typedef struct EPViewport {
+	float	x;
+	float	y;
+	float	width;
+	float	height;
+	float	minDepth;
+	float	maxDepth;
+} EPViewport;
+
+typedef enum {
+	ENGINE_PIPELINE_FLAG_DYNAMIC_VIEW = 0x1,
+	ENGINE_PIPELINE_FLAG_FACE_CLOCKWISE = 0x2,
+};
