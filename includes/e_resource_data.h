26e97d704e210316c9e2e06b56a6c4f82b20052f
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2024-02-01 18:42:35 +0400

    evce

--- includes/e_resource_data.h
+++ includes/e_resource_data.h
@@ -2,3 +2,13 @@
 #define E_RESOURCE_DATA_H
 
 #include "engine_includes.h"
+
+extern vec2 viewSize;
+extern vec2 diffSize;
+
+extern int WIDTH;
+extern int HEIGHT;
+
+extern char app_name[256];
+
+extern void* e_var_current_entry;
