5e05e2b18ffd040c6e71c8c1a9da4f437c07205b
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2024-02-01 22:14:07 +0400

    resource data 1

--- includes/e_resource_data.h
+++ includes/e_resource_data.h
@@ -12,3 +12,43 @@ extern int HEIGHT;
 extern char app_name[256];
 
 extern void* e_var_current_entry;
+
+extern void* cam2D;
+extern void* cam3D;
+
+extern void* dir_shadow_array;
+extern uint32_t num_dir_shadows;
+
+extern void* point_shadow_array;
+extern uint32_t num_point_shadows;
+
+extern void* spot_shadow_array;
+extern uint32_t num_point_shadows;
+
+extern void* geom_texture;
+extern void* geom_uniform;
+extern void* geom_geometry;
+
+extern GeometrySBO geometrySBO;
+
+extern char* rootDirPath;
+
+extern void* e_var_images;
+extern int e_var_num_images;
+
+extern void** e_var_lights;
+extern int e_var_num_lights;
+
+extern Fontcache* e_var_fonts;
+extern uint32_t e_var_num_fonts;
+
+extern int define_font_loaded;
+
+extern const char* deviceExtensions[];
+extern uint32_t num_dev_extensions;
+
+extern const FontIndexes fontIndexes[];
+
+#endif
+
+
