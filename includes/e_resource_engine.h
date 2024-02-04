20d11328592538a303217edd8eba9dd7e7188292
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2024-02-05 00:49:32 +0400

    include

--- includes/e_resource_engine.h
+++ includes/e_resource_engine.h
@@ -1,2 +1,12 @@
 #ifndef E_RESOURCE_ENGINE_H
 #define E_RESOURCE_ENGINE_H
+
+#include "engine_includes.h"
+
+extern bool enableValidationLayers;
+
+extern EIExtent2D swapChainExtent;
+
+extern void* e_window;
+extern void* instance;
+extern void* debugMessenger;
