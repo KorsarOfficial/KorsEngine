4b546b61c59c9888b7136c1c6c80af2dbf0155c6
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-02-17 15:39:31 +0400

    shadow fix

---
+++ includes/e_resource_export.h
@@ -0,0 +1,15 @@
+#ifndef E_RESOURCE_EXPORT_H
+#define E_RESOURCE_EXPORT_H
+
+extern char _binary_shaders_model_frag_spv_end;
+extern int _binary_shaders_model_frag_spv_size;
+extern char _binary_shaders_model_frag_spv_start;
+extern char _binary_shaders_model_vert_spv_end;
+extern int _binary_shaders_model_vert_spv_size;
+extern char _binary_shaders_model_vert_spv_start;
+extern char _binary_shaders_model_shadow_frag_spv_end;
+extern int _binary_shaders_model_shadow_frag_spv_size;
+extern char _binary_shaders_model_shadow_frag_spv_start;
+extern char _binary_shaders_model_shadow_vert_spv_end;
+extern int _binary_shaders_model_shadow_vert_spv_size;
+extern char _binary_shaders_model_shadow_vert_spv_start;
