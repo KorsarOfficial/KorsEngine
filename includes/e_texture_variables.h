b31bf4b5f5a47652112047220bfe1494fde2b1ef
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-02-20 22:40:19 +0100

    imagefile data 2d

--- includes/e_texture_variables.h
+++ includes/e_texture_variables.h
@@ -2,3 +2,46 @@
 #define E_TEXTURE_VARIABLES_H
 
 #include "engine.includes.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+	#endif
+
+	typedef enum {
+		ENGINE_TEXTURE2D_FLAG_GENERATED = 0x1,
+		ENGINE_TEXTURED2D_FLAG_VIEW = 0x2
+	} EngineTexture2DFlag;
+
+	typedef struct {
+		char* path;
+		char* buffer;
+		char* data;
+		uint32_t buff_size;
+		uint32_t texHeight;
+		uint32_t texWidth;
+		uint32_t texChannels;
+		uint32_t mip_levels;
+	} ImageFileData;
+
+	typedef struct {
+		void* textureImage;
+		void* textureImageMemory;
+		void* textureImageView;
+		void* textureSampler;
+		uint32_t textureType;
+		uint32_t imageLayout;
+		ImageFileData image_data;
+		uint32_t flags;
+	} Texture2D;
+
+	typedef struct {
+		char path[2048];
+		Texture2D texture;
+	} engine_buffered_image;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TEXTURE_VARIABLES_H
