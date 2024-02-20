9353082fd7f9d24e885a6a027e98d49173601cb2
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-02-20 21:33:43 +0400

    debug

--- includes/e_resource_export.h
+++ includes/e_resource_export.h
@@ -69,24 +69,24 @@ extern char _binary_shaders_depth_vert_omni_spv_end;
 extern int _binary_shaders_depth_vert_omni_spv_size;
 extern char _binary_shaders_depth_vert_omni_spv_start;
 
-extern char _binary_shaders - depth_frag_spot_spv_end;
+extern char _binary_shaders_depth_frag_spot_spv_end;
 extern int _binary_shaders_depth_frag_spot_spv_size;
-extern char _binary - shaders_depth - frag_spot_spv_start;
-extern char _binary_depth_vert_spot_spv_end;
+extern char _binary_shaders_depth_frag_spot_spv_start;
+extern char _binary_shaders_depth_vert_spot_spv_end;
 extern int _binary_shaders_depth_vert_spot_spv_size;
-extern char - binary_shaders_depth_vert_spot_spv_start;
+extern char _binary_shaders_depth_vert_spot_spv_start;
 
 extern char _binary_shaders_terrain_frag_spv_end;
-extern int  _binary_shaders_terrain_frag_spv_size;
+extern int _binary_shaders_terrain_frag_spv_size;
 extern char _binary_shaders_terrain_frag_spv_start;
 extern char _binary_shaders_terrain_vert_spv_end;
 extern int _binary_shaders_terrain_vert_spv_size;
 extern char _binary_shaders_terrain_vert_spv_start;
-extern char _binary_shaders_terrain_stesc_spv_end;
+extern char _binary_shaders_terrain_tesc_spv_end;
 extern int _binary_shaders_terrain_tesc_spv_size;
 extern char _binary_shaders_terrain_tesc_spv_start;
 extern char _binary_shaders_terrain_tese_spv_end;
-extern int _binary_shaders_terrain_tese_spv_end;
+extern int _binary_shaders_terrain_tese_spv_size;
 extern char _binary_shaders_terrain_tese_spv_start;
 extern char _binary_shaders_terrain_shadow_frag_spv_end;
 extern int _binary_shaders_terrain_shadow_frag_spv_size;
@@ -95,27 +95,26 @@ extern char _binary_shaders_terrain_shadow_vert_spv_end;
 extern int _binary_shaders_terrain_shadow_vert_spv_size;
 extern char _binary_shaders_terrain_shadow_vert_spv_start;
 
-extern char _binary_shaders_terrain_shadow_tree_frag_spv_end;
+extern char _binary_shaders_tree_frag_spv_end;
 extern int _binary_shaders_tree_frag_spv_size;
-extern char _binary_shaders_treee_frag_spv_start;
+extern char _binary_shaders_tree_frag_spv_start;
 extern char _binary_shaders_tree_vert_spv_end;
-extern int binary_shaders_tree_vert_spv_size;
-extern char _binary_shaders_treee_vert_spv_start;
+extern int _binary_shaders_tree_vert_spv_size;
+extern char _binary_shaders_tree_vert_spv_start;
 
 extern char _binary_shaders_tree_instance_frag_spv_end;
 extern int _binary_shaders_tree_instance_frag_spv_size;
 extern char _binary_shaders_tree_instance_frag_spv_start;
 extern char _binary_shaders_tree_instance_vert_spv_end;
-extern int - binary - shaders_tree_instance_vert_spv_size;
+extern int _binary_shaders_tree_instance_vert_spv_size;
 extern char _binary_shaders_tree_instance_vert_spv_start;
 
 extern char _binary_shaders_tree_sdf_frag_spv_end;
-extern int _binary_shadows_tree_sf_frag_spv_size;
+extern int _binary_shaders_tree_sdf_frag_spv_size;
 extern char _binary_shaders_tree_sdf_frag_spv_start;
 extern char _binary_shaders_tree_sdf_vert_spv_end;
 extern int _binary_shaders_tree_sdf_vert_spv_size;
 extern char _binary_shaders_tree_sdf_vert_spv_start;
-extern char _binary_shaders_tree_sdf_vert_spv_end;
 
 extern char _binary_shaders_image_frag_spv_end;
 extern int _binary_shaders_image_frag_spv_size;
@@ -123,3 +122,117 @@ extern char _binary_shaders_image_frag_spv_start;
 extern char _binary_shaders_image_vert_spv_end;
 extern int _binary_shaders_image_vert_spv_size;
 extern char _binary_shaders_image_vert_spv_start;
+
+extern char _binary_shaders_sky_sky_frag_spv_end;
+extern int _binary_shaders_sky_sky_frag_spv_size;
+extern char _binary_shaders_sky_sky_frag_spv_start;
+extern char _binary_shaders_sky_sky_vert_spv_end;
+extern int _binary_shaders_sky_sky_vert_spv_size;
+extern char _binary_shaders_sky_sky_vert_spv_start;
+
+extern char _binary_shaders_sky_stars_frag_spv_end;
+extern int _binary_shaders_sky_stars_frag_spv_size;
+extern char _binary_shaders_sky_stars_frag_spv_start;
+extern char _binary_shaders_sky_stars_vert_spv_end;
+extern int _binary_shaders_sky_stars_vert_spv_size;
+extern char _binary_shaders_sky_stars_vert_spv_start;
+
+extern char _binary_shaders_sky_atmospheric_frag_spv_end;
+extern int _binary_shaders_sky_atmospheric_frag_spv_size;
+extern char _binary_shaders_sky_atmospheric_frag_spv_start;
+extern char _binary_shaders_sky_atmospheric_vert_spv_end;
+extern int _binary_shaders_sky_atmospheric_vert_spv_size;
+extern char _binary_shaders_sky_atmospheric_vert_spv_start;
+
+extern char _binary_shaders_sprite_frag_spv_end;
+extern int _binary_shaders_sprite_frag_spv_size;
+extern char _binary_shaders_sprite_frag_spv_start;
+extern char _binary_shaders_sprite_vert_spv_end;
+extern int _binary_shaders_sprite_vert_spv_size;
+extern char _binary_shaders_sprite_vert_spv_start;
+
+extern char _binary_shaders_particle_frag2D_spv_end;
+extern int _binary_shaders_particle_frag2D_spv_size;
+extern char _binary_shaders_particle_frag2D_spv_start;
+extern char _binary_shaders_particle_vert2D_spv_end;
+extern int _binary_shaders_particle_vert2D_spv_size;
+extern char _binary_shaders_particle_vert2D_spv_start;
+
+extern char _binary_shaders_particle_frag3D_spv_end;
+extern int _binary_shaders_particle_frag3D_spv_size;
+extern char _binary_shaders_particle_frag3D_spv_start;
+extern char _binary_shaders_particle_vert3D_spv_end;
+extern int _binary_shaders_particle_vert3D_spv_size;
+extern char _binary_shaders_particle_vert3D_spv_start;
+
+extern char _binary_shaders_3d_object_frag_spv_end;
+extern int _binary_shaders_3d_object_frag_spv_size;
+extern char _binary_shaders_3d_object_frag_spv_start;
+extern char _binary_shaders_3d_object_vert_spv_end;
+extern int _binary_shaders_3d_object_vert_spv_size;
+extern char _binary_shaders_3d_object_vert_spv_start;
+extern char _binary_shaders_3d_object_shadow_frag_spv_end;
+extern int _binary_shaders_3d_object_shadow_frag_spv_size;
+extern char _binary_shaders_3d_object_shadow_frag_spv_start;
+extern char _binary_shaders_3d_object_shadow_vert_spv_end;
+extern int _binary_shaders_3d_object_shadow_vert_spv_size;
+extern char _binary_shaders_3d_object_shadow_vert_spv_start;
+extern char _binary_shaders_3d_object_instance_frag_spv_end;
+extern int _binary_shaders_3d_object_instance_frag_spv_size;
+extern char _binary_shaders_3d_object_instance_frag_spv_start;
+extern char _binary_shaders_3d_object_instance_vert_spv_end;
+extern int _binary_shaders_3d_object_instance_vert_spv_size;
+extern char _binary_shaders_3d_object_instance_vert_spv_start;
+
+extern char _binary_shaders_water_frag_spv_end;
+extern int _binary_shaders_water_frag_spv_size;
+extern char _binary_shaders_water_frag_spv_start;
+extern char _binary_shaders_water_vert_spv_end;
+extern int _binary_shaders_water_vert_spv_size;
+extern char _binary_shaders_water_vert_spv_start;
+
+extern char _binary_shaders_3d_object_line_frag_spv_end;
+extern int _binary_shaders_3d_object_line_frag_spv_size;
+extern char _binary_shaders_3d_object_line_frag_spv_start;
+extern char _binary_shaders_3d_object_line_vert_spv_end;
+extern int _binary_shaders_3d_object_line_vert_spv_size;
+extern char _binary_shaders_3d_object_line_vert_spv_start;
+
+extern char _binary_shaders_gui_widget_frag_spv_end;
+extern int _binary_shaders_gui_widget_frag_spv_size;
+extern char _binary_shaders_gui_widget_frag_spv_start;
+extern char _binary_shaders_gui_widget_vert_spv_end;
+extern int _binary_shaders_gui_widget_vert_spv_size;
+extern char _binary_shaders_gui_widget_vert_spv_start;
+
+extern char _binary_shaders_gui_painter_frag_spv_end;
+extern int _binary_shaders_gui_painter_frag_spv_size;
+extern char _binary_shaders_gui_painter_frag_spv_start;
+extern char _binary_shaders_gui_painter_vert_spv_end;
+extern int _binary_shaders_gui_painter_vert_spv_size;
+extern char _binary_shaders_gui_painter_vert_spv_start;
+
+extern char _binary_shaders_gui_widget_window_frag_spv_end;
+extern int _binary_shaders_gui_widget_window_frag_spv_size;
+extern char _binary_shaders_gui_widget_window_frag_spv_start;
+extern char _binary_shaders_gui_widget_window_vert_spv_end;
+extern int _binary_shaders_gui_widget_window_vert_spv_size;
+extern char _binary_shaders_gui_widget_window_vert_spv_start;
+
+extern char _binary_fonts_null_normal_otf_end;
+extern int _binary_fonts_null_normal_otf_size;
+extern char _binary_fonts_null_normal_otf_start;
+
+extern char _binary_fonts_fantazer_normal_ttf_end;
+extern int _binary_fonts_fantazer_normal_ttf_size;
+extern char _binary_fonts_fantazer_normal_ttf_start;
+
+extern char _binary_fonts_RobotoBlack_ttf_end;
+extern int _binary_fonts_RobotoBlack_ttf_size;
+extern char _binary_fonts_RobotoBlack_ttf_start;
+
+extern char _binary_fonts_TenorSans_ttf_end;
+extern int _binary_fonts_TenorSans_ttf_size;
+extern char _binary_fonts_TenorSans_ttf_start;
+
+#endif
