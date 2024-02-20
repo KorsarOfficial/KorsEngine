4432f52c61de6f028da1e519d1e34312be9a83ca
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-02-21 00:48:18 +0400

    DEF/INC

---
+++ includes/e_texture.h
@@ -0,0 +1,13 @@
+#ifndef E_TEXTURE_H
+#define E_TEXTURE_H
+
+#include "engine.includes.h"
+
+#include "e_blue_print.h"
+
+#include "e_texture_variables.h"
+
+#include "e_tools.h"
+
+#define EMPTY_IMAGE_WIDTH 100
+#define EMPTY_IMAGE_HEIGHT 100
