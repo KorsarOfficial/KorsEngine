457c881e1b0177195cfa4fff94f3b173222994b2
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-02-21 00:53:30 +0400

    texture_flag

--- includes/e_texture.h
+++ includes/e_texture.h
@@ -11,3 +11,18 @@
 
 #define EMPTY_IMAGE_WIDTH 100
 #define EMPTY_IMAGE_HEIGHT 100
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    typedef enum {
+        ENGINE_TEXTURE_FLAG_SPECIFIC = 0x1,
+        ENGINE_TEXTURE_FLAG_URGB = 0x2,
+        ENGINE_TEXTURE_FLAG_R16 = 0x4,
+        ENGINE_TEXTURE_FLAG_R16_UINT = 0x8,
+        ENGINE_TEXTURE_FLAG_R32 = 0x16,
+        ENGINE_TEXTURE_FLAG_R32_UINT = 0x32,
+        ENGINE_TEXTURE_FLAG_SRGB = 0x64
+    } EngineTextureFlags;
