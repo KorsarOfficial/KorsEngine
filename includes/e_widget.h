75d264779879867311e7599366f74101e5fbb445
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-02-21 18:22:17 +0100

    inc widget.h

---
+++ includes/e_widget.h
@@ -0,0 +1,18 @@
+#ifndef E_WIDGET_H
+#define E_WIDGET_H
+
+#include "engine_includes.h"
+
+#include "gameObject2D.h"
+
+#define MAX_WIDGET_MASKS 64
+#define MAX_WIDGET_DRAW 256
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+	typedef enum {
+
+	};
+}
