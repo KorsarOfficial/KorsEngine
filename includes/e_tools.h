e4c36f6d08884fe158a07e4f2e7f68f4e55dd887
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-02-21 14:57:40 +0400

    debug

--- includes/e_tools.h
+++ includes/e_tools.h
@@ -12,7 +12,8 @@ extern "C"
 #endif
 
 	void* beginSingleTimeCommands();
-	void endSingleTimeCommands(void* commandBuffer); // InitGrass
+
+	void endSingleTimeCommands(void* commandBuffer);
 
 	bool isComplete(QueueFamilyIndices self);
 
@@ -24,19 +25,21 @@ extern "C"
 	void* createShaderModule(shader shdr);
 
 	void InitGrass3D(vertexParam* vParam, indexParam* iParam);
-	void InitPlane3D(vertexParam* vParam, indexParam* iParam, int rows, int colms);
+	void InitPlane3D(vertexParam* vParam, indexParam* iParam, int rows, int colmns);
 	void InitTerrain(vertexParam* vParam, indexParam* iParam, void* param);
-	int SphereGenerator3D(vertexParam* vParam, indexParam* iParam_radius, float radius, int stackCount, int sectorCount);
-	void ConeGenerator(vertexParam* vParam, indexParam* iParam, float radius);
-	int Cubesphere(vertexParam*Vparam*iParam, float radius, int vertexCountPerRow);
+	int SphereGenerator3D(vertexParam* vParam, indexParam* iParam, float radius, int stackCount, int sectorCount);
+	void ConeGenerator(vertexParam* vParam, indexParam* iParam, const float height, int stackCount, int sectorCount);
+	int IcoSphereGenerator(vertexParam* vParam, indexParam* iParam, float radius);
+	int Cubesphere(vertexParam* vParam, indexParam* iParam, float radius, int vertexCountPerRow);
 
 	void ToolsCreateDepthResources();
 	void ToolsTransitionImageLayout(void* image, uint32_t format, uint32_t oldLayout, uint32_t newLayout, uint32_t mip_levels);
 	void ToolsTransitionImageLayoutLite(void* image, uint32_t oldLayout, uint32_t newLayout, uint32_t aspect_mask, uint32_t mip_levels);
+	void ToolsCopyImageToBuffer(void* buffer, void* image, uint32_t width, uint32_t height);
 	void ToolsCopyBufferToImage(void* buffer, void* image, uint32_t width, uint32_t height);
-	void ToolsCopyImage(void* cmdBuffer, void* cmdBuffer, void* srcimageId, void* dstImageId, uint32_t width, uint32_t height
+	void ToolsCopyImage(void* cmdBuffer, void* srcImageId, void* dstImageId, uint32_t width, uint32_t height);
 
-		float dot_grad(int hash, float xf);
+	float dot_grad(int hash, float xf);
 	float dot_grad2(int hash, float xf, float yf);
 
 	vec3 perp(const vec3 v);
@@ -62,6 +65,7 @@ extern "C"
 	void ToolsAddStrings(char* out, int buff_size, char* s1, char* s2);
 	bool ToolsCmpStrings(char* in, char* s1);
 	int ToolsStr32BitLength(uint32_t* text);
+	int ToolsStringToUInt32(uint32_t* dest, const char* src);
 	int ToolsUInt32ToString(char* dest, const uint32_t* src);
 
 	void* ToolsLoadImageFromFile(size_t* len, char* filepath);
@@ -70,4 +74,4 @@ extern "C"
 }
 #endif
 
-#endif // TOOLS_H
+#endif
