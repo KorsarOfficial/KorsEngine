df6a2fee51175eaf018f8b07144430571178760a
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-02-21 18:59:42 +0100

    inc

---
+++ includes/e_widget_button.h
@@ -0,0 +1,10 @@
+#ifndef E_WIDGET_BUTTON_H
+#define E_WIDGET_BUTTON_H
+
+#include "engine_includes.h"
+
+#include "e_widget.h"
+#include "e_widget_image.h"
+#include "e_widget_text.h"
+
+#include <wchar.h>
