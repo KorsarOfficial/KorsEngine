a9017b4e3ab98331c8e07f57b0780180323f0491
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-02-23 02:44:29 +0400

    ex C + inc

---
+++ includes/e_widget_combobox.h
@@ -0,0 +1,10 @@
+#ifndef E_WIDGET_COMBOBOX_H
+#define E_WIDGET_COMBOBOX_H
+
+#include "e_widget.h"
+#include "e_widget_button.h"
+#include "e_widget_list.h"
+
+#ifdef __cplusplus
+extern "C"
+#endif // !
