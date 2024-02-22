6a95bb85ca72dd618d16a993d4dd0d076ffaa431
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-02-23 02:55:23 +0400

    entrywidgetUpdate

--- includes/e_widget_entry.h
+++ includes/e_widget_entry.h
@@ -6,6 +6,28 @@
 
 #ifdef __cplusplus
 extern "C"
+{
+#endif
+	typedef struct {
+		EWidget widget;
+		EWidgetText text;
+		uint32_t num_texts;
+		uint32_t curr_texts;
+		char** buffers;
+		uint32_t num_lines;
+		uint32_t curr_line;
+		int currPos;
+		float width;
+		float height;
+	} EWidgetEntry;
+
+	extern bool e_ctrl_press, e_c_press, e_v_press, e_pasted;
+
+	int EntryWidgetDestroyDefault(EWidgetEntry* entry);
+
+	void EntryWidgetInit(EWidgetEntry* entry, int fontSize, DrawParam* dParam, EWidget* parent);
+	void EntryWidgetUpdate();
+}
 #endif // __cplusplus
 
 #endif // !1
