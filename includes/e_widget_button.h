aac0a9704679c6e4eb469d1faf2ef58196069acd
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-02-22 19:09:41 +0400

    debug

--- includes/e_widget_button.h
+++ includes/e_widget_button.h
@@ -25,6 +25,7 @@ extern "C"
     void ButtonWidgetSetText(EWidgetButton* button, const char* text);
     void ButtonWidgetSetColor(EWidgetButton* button, float r, float g, float b);
     void ButtonWidgetSetImage(EWidgetButton* button, char* path, DrawParam* dParam);
+
 #ifdef __cplusplus
 }
 #endif
