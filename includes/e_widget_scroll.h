81636b35b38b9faf2e0b45395ab843b22082d23a
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-02-23 04:02:47 +0400

    ewidgetscroll

--- includes/e_widget_scroll.h
+++ includes/e_widget_scroll.h
@@ -5,3 +5,14 @@
 
 #ifdef __cplusplus
 extern "C"
+{
+#endif
+
+	typedef struct {
+		EWidget widget;
+		EWidget scroll;
+		uint32_t width;
+		uint32_t height;
+		uint32_t scroll_size;
+	} EWidgetScroll;
+}
