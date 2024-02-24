fae666537f06d42d1f0adb85f23f9a060e4474a0
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-02-24 01:41:06 +0100

    ENGINE_MOUSE_BUTTON_1 = 0

--- includes/engine.h
+++ includes/engine.h
@@ -8,7 +8,9 @@ extern "C"
 [
 #endif
 
-#define ENGINE_RELEASE		0
-#define ENGINE_PRESS		1
-#define ENGINE_REPEAT		2
+#define ENGINE_RELEASE					0
+#define ENGINE_PRESS					1
+#define ENGINE_REPEAT					2
+
+#define ENGINE_MOUSE_BUTTON_1			0
 ]
