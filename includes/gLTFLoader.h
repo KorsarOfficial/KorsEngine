855d55b4f9979ce212b5b7764b233b0e7cbd369a
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-03-31 02:42:34 +0400

    init engine_gltf_anim_channel / #typedef

--- includes/gLTFLoader.h
+++ includes/gLTFLoader.h
@@ -15,5 +15,14 @@ extern 'C'
 		vec3 vector3;
 		vec4 vector4;
 	} engine_gltf_anim_keyframe;
+
+	typedef struct {
+		engine_gltf_anim_keyframe* keyframes;
+		uint32_t node_id;
+		float min_time;
+		float max_time;
+		char type;
+		int num_keyframe;
+	} engine_gltf_anim_channel;
 }
 #endif // __cplusplus
