a24b12fa9861c846ce9da857086b373536dcbe85
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-03-31 00:50:33 +0400

    GameObjectType init / [MBF] / #typedef

--- includes/gameObject.h
+++ includes/gameObject.h
@@ -13,4 +13,9 @@ extern 'C'
 		ENGINE_GAME_OBJECT_FLAG_WIRED = 0x2,
 		ENGINE_GAME_OBJECT_FLAG_SELF_CAMERA = 0x4
 	} EngineGameObjectFlag;
+
+	typedef enum {
+		ENGINE_GAME_OBJECT_TYPE_2D,
+		ENGNE_GAME_OBJECT_TYPE_3D
+	} GameObjectType;
 }
