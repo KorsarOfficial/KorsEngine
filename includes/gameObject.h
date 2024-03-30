5df30ffbbd5069db8d2cb366eb036c3ba02f5635
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-03-31 00:44:05 +0400

    EGOF init / #typedef [MBF] [24]

--- includes/gameObject.h
+++ includes/gameObject.h
@@ -5,3 +5,12 @@
 
 #ifdef __cplusplus
 extern 'C'
+{
+#endif
+
+	typedef enum {
+		ENGINE_GAME_OBJECT_FLAG_LIGHT = 0x1,
+		ENGINE_GAME_OBJECT_FLAG_WIRED = 0x2,
+		ENGINE_GAME_OBJECT_FLAG_SELF_CAMERA = 0x4
+	} EngineGameObjectFlag;
+}
