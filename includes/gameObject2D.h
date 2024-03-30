4f9a642c0ad5ef4253653219e19a44174a5c28b2
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-03-31 01:31:01 +0400

    init #include "gameObject.h" #include "graphicsObject.h" #include "e_transform.h"
    #include "e_descriptor.h"
    #include "pipeline.h"
    #include "e_texture.h"

---
+++ includes/gameObject2D.h
@@ -0,0 +1,12 @@
+#ifndef GAMEOBJECT2D_H
+#define GAMEOBJECT2D_H
+
+#include "engine_includes.h"
+
+#include "gameObject.h"
+#include "graphicsObject.h"
+#include "e_transform.h"
+
+#include "e_descriptor.h"
+#include "pipeline.h"
+#include "e_texture.h"
