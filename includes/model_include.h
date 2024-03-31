94b5de83e37896a18d7ed9780230c6aa0f30c132
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-03-31 22:08:56 +0400

    blank

--- includes/model_include.h
+++ includes/model_include.h
@@ -6,29 +6,29 @@
 #include "engine_includes.h"
 
 typedef struct {
-	mat4 inv_mat;
-	mat4 join_mat;
-	uint32_t id_node;
+    mat4 inv_mat;
+    mat4 join_mat;
+    uint32_t id_node;
 } join_mat_struct;
 
 typedef struct skin_vertex {
-	uint8_t bone_index[4];
-	uint8_t bone_weight[4];
+    uint8_t bone_index[4];
+    uint8_t bone_weight[4];
 } skin_vertex;
 
 typedef struct {
-	ModelVertex3D* verts;
-	uint32_t num_verts;
+    ModelVertex3D* verts;
+    uint32_t num_verts;
 
-	uint32_t* indices;
-	uint32_t num_indices;
+    uint32_t* indices;
+    uint32_t num_indices;
 
-	uint32_t* instance_node_indices;
-	size_t num_instances;
+    int32_t* instance_node_indices;
+    size_t num_instances;
 
-	GameObjectImage* image;
-	GameObjectImage* specular;
-	GameObjectImage* normal;
+    GameObjectImage* image;
+    GameObjectImage* specular;
+    GameObjectImage* normal;
 } engine_model_mesh;
 
-#endif // MODEL_INCLUDE_H
+#endif
