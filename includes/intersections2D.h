44aa15974360e5b72446f786599f80b9cff8989e
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-03-31 22:06:29 +0400

    blank

--- includes/intersections2D.h
+++ includes/intersections2D.h
@@ -13,8 +13,8 @@ extern "C"
 	bool IntersectGJK2D(GJKObject* gjk, void* obj1, void* obj2);
 	int IntersectLineToLine(vec2 p1, vec2 p2, vec2 p3, vec2 p4);
 	int IntersectionCircleTriangle(vec2 sPos, float r, vec2 p0, vec2 p1, vec2 p2, vec2* resPos, float* dist, float* depth, vec2* dir);
-	int IntersectionCircleCirlce(InterCircleParam* o1, InterCircleParam* o2, float* dist, float* depth, float* depth, vec2* dir);
-	int IntersectionCircleSquare(InterCircleParam* sph, InterSquareParam* o2, float* dist, float* depth, vec2* dir);
+	int IntersectionCircleCircle(InterCircleParam* o1, InterCircleParam* o2, float* dist, float* depth, vec2* dir);
+	int IntersectionCircleSquare(InterCircleParam* sph, InterSquareParam* box, float* dist, float* depth, vec2* dir);
 	int IntersectionSquareSquare(InterSquareParam* box1, InterSquareParam* box2, float* dist, float* depth, vec2* dir);
 	int IntersectionTriangleSquare(InterTriangleParam triangle, InterSquareParam* box);
 	int IntersectionSphapeSquare(void* obj1, InterSquareParam* square);
@@ -24,4 +24,4 @@ extern "C"
 }
 #endif
 
-#endif // INTERSECTIONS2D_H
+#endif
