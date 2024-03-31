880629ac8bd622d946d148ca7e58d60fc1f0af36
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-03-31 22:05:50 +0400

    debug

--- includes/graphicsObject.h
+++ includes/graphicsObject.h
@@ -11,29 +11,27 @@ extern "C"
 {
 #endif
 
-	typedef struct {
-		Blueprints blueprints;
-		GraphicItems gItems;
-		aboutShader aShader;
-		bool lock;
-		Shape shapes[MAX_SHAPES];
-		uint32_t num_shapes;
-	} GraphicsObject;
+    typedef struct {
+        Blueprints blueprints;
+        GraphicItems gItems;
+        aboutShader aShader;
+        bool lock;
+        Shape shapes[MAX_SHAPES];
+        uint32_t num_shapes;
+    } GraphicsObject;
 
-	void GraphicsObjectInit(GraphicsObject* graphObj, uint32_t type);
+    void GraphicsObjectInit(GraphicsObject* graphObj, uint32_t type);
 
-	void GraphicsObjectSetVertex(GraphicsObject* graphObj, void* vert, int vertCount, uint32_t type_v_size, uint32_t* inx, int indxCount, uint32_t type_i_size);
+    void GraphicsObjectSetVertex(GraphicsObject* graphObj, void* vert, int vertCount, uint32_t type_v_size, uint32_t* inx, int indxCount, uint32_t type_i_size);
 
-	void GraphicsObjectCreateDrawItems(GraphicsObject* graphObj);
-	void GraphicsObjectCleanPipelines(GraphicsObject* graphObj);
-	void GraphicsObjectClean(GraphicsObject* graphObj);
-	void GraphicsObjectDestroy(GraphicsObject* graphObj);
-
-	void GraphicsObjectSetShadersPath(GraphicsObject* graphObj, const char* vert, const char* frag);
+    void GraphicsObjectCreateDrawItems(GraphicsObject* graphObj);
+    void GraphicsObjectCleanPipelines(GraphicsObject* graphObj);
+    void GraphicsObjectClean(GraphicsObject* graphObj);
+    void GraphicsObjectDestroy(GraphicsObject* graphObj);
+    void GraphicsObjectSetShadersPath(GraphicsObject* graphOb, const char* vert, const char* frag);
 
 #ifdef __cplusplus
 }
-
 #endif
 
 #endif // GRAPHICSOBJECT_H
