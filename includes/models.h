375af7528de82b1c7b0eff2e1ffade6329c0cc3b
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-03-31 22:08:32 +0400

    blank

--- includes/models.h
+++ includes/models.h
@@ -14,52 +14,52 @@ extern "C"
 {
 #endif
 
-	typedef struct {
-		GraphicsObject graphObj;
-		GameObjectImage* diffuse;
-		GameObjectImage* specular;
-		GameObjectImage* normal;
-	} ModelStruct;
+    typedef struct {
+        GraphicsObject graphObj;
+        GameObjectImage* diffuse;
+        GameObjectImage* specular;
+        GameObjectImage* normal;
+    } ModelStruct;
 
-	typedef struct {
-		ModelStruct* models;
-		uint32_t num_models;
-		uint32_t id_node;
-		mat4 model;
-	} ModelNode;
+    typedef struct {
+        ModelStruct* models;
+        uint32_t num_models;
+        uint32_t id_node;
+        mat4 model;
+    } ModelNode;
 
-	typedef struct {
-		GameObject self;
-		Transform3D transform;
+    typedef struct {
+        GameObject self;
+        Transform3D transform;
 
-		ModelNode* nodes;
-		uint32_t num_draw_nodes;
+        ModelNode* nodes;
+        uint32_t num_draw_nodes;
 
-		mat4 join_mats[MAX_BONES];
+        mat4 join_mats[MAX_BONES];
 
-		void* obj;
-		uint32_t obj_size;
-	} ModelObject3D;
+        void* obj;
+        uint32_t obj_size;
+    } ModelObject3D;
 
-	void ModelModelBufferUpdate(ModelObject3D* mo, uint32_t indx_node, BluePrintDescriptor* descriptor);
-	void ModelLightMatrixUpdate(ModelObject3D* mo, uint32_t indx_node, BluePrintDescriptor* descriptor);
-	void ModelInvMatrixBuffer(ModelObject3D* mo, uint32_t indx_node, BluePrintDescriptor* descriptor);
-	void ModelDescriptorLightObjectsUpdate(ModelObject3D* mo, uint32_t indx_node, BluePrintDescriptor* descriptor);
+    void ModelModelBufferUpdate(ModelObject3D* mo, uint32_t indx_node, BluePrintDescriptor* descriptor);
+    void ModelLightMatrixUpdate(ModelObject3D* mo, uint32_t indx_node, BluePrintDescriptor* descriptor);
+    void ModelInvMatrixBuffer(ModelObject3D* mo, uint32_t indx_node, BluePrintDescriptor* descriptor);
+    void ModelDescriptorLightObjectsUpdate(ModelObject3D* mo, uint32_t indx_node, BluePrintDescriptor* descriptor);
 
-	void ModelDefaultDraw(ModelObject3D* mo, void* command);
-	void ModelDefaultUpdate(ModelObject3D* mo);
-	void ModelClean(ModelObject3D* mo);
-	void ModelDestroy(ModelObject3D* mo);
-	void ModelRecreate(ModelObject3D* mo);
+    void ModelDefaultDraw(ModelObject3D* mo, void* command);
+    void ModelDefaultUpdate(ModelObject3D* mo);
+    void ModelClean(ModelObject3D* mo);
+    void ModelRecreate(ModelObject3D* mo);
+    void ModelDestroy(ModelObject3D* mo);
 
-	void ModelSetLightEnable(void* obj, bool enable);
-	void ModelSetSelCameraEnable(void* obj, bool enable);
-	void ModelSetSomeViewport(ModelObject3D* mo, float x, float y, float height, float width);
+    void ModelSetLightEnable(void* obj, bool enable);
+    void ModelSetSelCameraEnable(void* obj, bool enable);
+    void ModelSetSomeViewport(ModelObject3D* mo, float x, float y, float height, float width);
 
-	void ModelDefaultInit(ModelStruct* model, DrawParam* dParam);
+    void ModelDefaultInit(ModelStruct* model, DrawParam* dParam);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // MODELS_H
+#endif
