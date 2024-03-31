0907e9bea5b410eb07c673df40250d02c48e9332
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-03-31 21:35:49 +0400

    Model.destroy / #void

--- includes/models.h
+++ includes/models.h
@@ -49,5 +49,5 @@ extern "C"
 	void ModelDefaultDraw(ModelObject3D* mo, void* command);
 	void ModelDefaultUpdate(ModelObject3D* mo);
 	void ModelClean(ModelObject3D* mo);
-
+	void ModelDestroy(ModelObject3D* mo);
 }
