64c1fb5b84b729967f6afc95a0d303f8683c39c5
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-03-31 21:58:12 +0400

    debug

--- includes/gameObject2D.h
+++ includes/gameObject2D.h
@@ -16,32 +16,30 @@ extern "C"
 {
 #endif
 
-	typedef struct {
-		GameObject self;
-		GraphicsObject graphObj;
-		Transform2D transform;
-		GameObjectImage* image;
-		char name[256];
-	} GameObject2D;
+    typedef struct {
+        GameObject self;
+        GraphicsObject graphObj;
+        Transform2D transform;
+        GameObjectImage* image;
+        char name[256];
+    } GameObject2D;
 
-	void GameObject2DTransformBufferUpdate(GameObject2D* go, BluePrintDescriptor *descriptor);
-	void GameObject2DImageBuffer(GameObject2D* go, BluePrintDescriptor* descriptor);
+    void GameObject2DTransformBufferUpdate(GameObject2D* go, BluePrintDescriptor* descriptor);
+    void GameObject2DImageBuffer(GameObject2D* go, BluePrintDescriptor* descriptor);
+    void GameObject2DDefaultUpdate(GameObject2D* go);
+    void GameObject2DDefaultDraw(GameObject2D* go, void* command);
+    void GameObject2DInitDraw(GameObject2D* go);
+    void GameObject2DInit(GameObject2D* go);
+    void GameObject2DClean(GameObject2D* go);
+    void GameObject2DRecreate(GameObject2D* go);
+    void GameObject2DDestroy(GameObject2D* go);
 
-	void GameObject2DDefaultUpdate(GameObject2D* go);
-	void GameObject2DDefaultDraw(GameObject2D* go, void* command);
+    void GameObject2DAddSettingPipeline(GameObject2D* go, uint32_t indx_pack, void* arg);
 
-	void GameObject2DInitDraw(GameObject2D* go);
-	void GameObject2DInit(GameObject2D* go);
-	void GameObject2DClean(GameObject2D* go);
-	void GameObject2DRecreate(GameObject2D* go);
-	void GameObject2DDestroy(GameObject2D* go);
-
-	void GameObject2DAddSettingPipeline(GameObject* go, uint32_t indx_pack, void* args);
-
-	vec2 GameObject2DGetSize(GameObject2D* go);
+    vec2 GameObject2DGetSize(GameObject2D* go);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // 2D
+#endif
