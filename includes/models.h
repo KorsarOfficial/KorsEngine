842fbda4fa7e485212dc5abc195f5d1af569a432
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-03-31 21:25:42 +0400

    ex "C"

---
+++ includes/models.h
@@ -0,0 +1,13 @@
+#ifndef MODELS_H
+#define MODELS_H
+
+#include "engine_includes.h"
+
+#include "model_include.h"
+
+#include "gameObject.h"
+#include "graphicsObject.h"
+#include "e_transform.h"
+
+#ifdef __cplusplus
+extern "C"
