b9e2ad81f7ee527daa6241ee8e99dab15f1fb579
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-03-31 21:57:49 +0400

    debug blank fix

--- includes/gameObject.h
+++ includes/gameObject.h
@@ -8,44 +8,44 @@ extern "C"
 {
 #endif
 
-	typedef enum {
-		ENGINE_GAME_OBJECT_FLAG_LIGHT = 0x1,
-		ENGINE_GAME_OBJECT_FLAG_WIRED = 0x2,
-		ENGINE_GAME_OBJECT_FLAG_SELF_CAMERA = 0x4
-	} EngineGameObjectFlag;
+    typedef enum {
+        ENGINE_GAME_OBJECT_FLAG_LIGHT = 0x1,
+        ENGINE_GAME_OBJECT_FLAG_WIRED = 0x2,
+        ENGINE_GAME_OBJECT_FLAG_SELF_CAMERA = 0x4
+    } EngineGmaeObjectFlag;
 
-	typedef enum {
-		ENGINE_GAME_OBJECT_TYPE_2D,
-		ENGNE_GAME_OBJECT_TYPE_3D
-	} GameObjectType;
+    typedef enum {
+        ENGINE_GAME_OBJECT_TYPE_2D,
+        ENGINE_GAME_OBJECT_TYPE_3D
+    } GameObjectType;
 
-	typedef struct {
-		void* InitPoint;
-		void* UpdatePoint;
-		void* DrawPoint;
-		void* CleanPoint;
-		void* RecreatePoint;
-		void* DestroyPoint;
-		uint32_t obj_type;
-		uint32_t flags;
-	} GameObject;
+    typedef struct {
+        void* InitPoint;
+        void* UpdatePoint;
+        void* DrawPoint;
+        void* CleanPoint;
+        void* RecreatePoint;
+        void* DestroyPoint;
+        uint32_t obj_type;
+        uint32_t flags;
+    } GameObject;
 
-	void GameObjectSetInitFunc(GameObject* go, void* func);
-	void GameObjectSetUpdateFunc(GameObject* go, void* func);
-	void GameObjectSetDrawFunc(GameObject* go, void* func);
-	void GameObjectSetCleanFunc(GameObject* go, void* func);
-	void GameObjectSetRecreateFunc(GameObject* go, void* func);
-	void GameObjectSetDestroyFunc(GameObject* go, void* func);
+    void GameObjectSetInitFunc(GameObject* go, void* func);
+    void GameObjectSetUpdateFunc(GameObject* go, void* func);
+    void GameObjectSetDrawFunc(GameObject* go, void* func);
+    void GameObjectSetCleanFunc(GameObject* go, void* func);
+    void GameObjectSetRecreateFunc(GameObject* go, void* func);
+    void GameObjectSetDestroyFunc(GameObject* go, void* func);
 
-	void GameObjectInit(GameObject* go);
-	void GameObjectUpdate(GameObject* go);
-	void GameObjectDraw(GameObject* go, void* command);
-	void GameObjectClean(GameObject* go);
-	void GameObjectRecreate(GameObject* go);
-	void GameObjectDestroy(GameObject* go);
+    void GameObjectInit(GameObject* go);
+    void GameObjectUpdate(GameObject* go);
+    void GameObjectDraw(GameObject* go, void* command);
+    void GameObjectClean(GameObject* go);
+    void GameObjectRecreate(GameObject* go);
+    void GameObjectDestroy(GameObject* go);
 
-#ifdef
+#ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif 
