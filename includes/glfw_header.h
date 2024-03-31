f54e392feb2d61f8d5563e29d49275a9df834931
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-03-31 15:39:08 +0400

    glfw create window / #typedef

--- includes/glfw_header.h
+++ includes/glfw_header.h
@@ -384,3 +384,4 @@ typedef void (*GLFWmousebuttonfun)(GLFWwindow*, int, int, int);
 typedef void (*GLFWkeyfun)(GLFWwindow* int, int, int, int);
 
 typedef GLFWcursorposfun(*PFN_glfwSetCursorPosCallback)(GLFWwindow* window, GLFWcursorposfun callback);
+typedef GLFWwindow* (*PFN_glfwCreateWindow)(int width, int height, const char* title, GLFWmonitor* monitor, GLFWmonitor* share);
