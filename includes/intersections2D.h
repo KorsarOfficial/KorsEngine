239b101ccc432187fd5969cc25c73e17215374d1
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-03-31 18:20:09 +0400

    IntersectionCircleSquare int / #int

--- includes/intersections2D.h
+++ includes/intersections2D.h
@@ -13,5 +13,7 @@ extern "C"
 	bool IntersectGJK2D(GJKObject* gjk, void* obj1, void* obj2);
 	int IntersectLineToLine(vec2 p1, vec2 p2, vec2 p3, vec2 p4);
 	int IntersectionCircleTriangle(vec2 sPos, float r, vec2 p0, vec2 p1, vec2 p2, vec2* resPos, float* dist, float* depth, vec2* dir);
+	int IntersectionCircleCirlce(InterCircleParam* o1, InterCircleParam* o2, float* dist, float* depth, float* depth, vec2* dir);
+	int IntersectionCircleSquare(InterCircleParam* sph, InterSquareParam* o2, float* dist, float* depth, vec2* dir);
 
 }
