ede540d8f254c41e8b7936940acc0c9181ca9726
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-04-01 13:41:51 +0400

    AABB init / #int

--- includes/rayintersections3D.h
+++ includes/rayintersections3D.h
@@ -12,4 +12,6 @@ extern "C"
 
 	int Intersect3DSegmentCylinder(InterRay3DParam* ray, vec3 p, vec3 q, float r, float* t, vec3 q_res);
 	
+	int Intersect3DRayAABB(InterRay3DParam* ray, vec3 obj_min, vec3 obj_max, float* tmin, flaot* tmax, vec3* q);
+
 }
