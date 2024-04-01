9aa03cb0493638b536c8af3f63b128d1795be779
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-04-01 13:02:22 +0400

    #inc

---
+++ includes/pipeline.h
@@ -0,0 +1,11 @@
+#ifndef PIPELINE_H
+#define PIPELINE_H
+
+#include "engine_includes.h"
+
+#include "e_pipeline_variables.h"
+
+#include "graphicsObject.h"
+
+#include "e_tools.h"
+#include 'e_descriptor.h'
