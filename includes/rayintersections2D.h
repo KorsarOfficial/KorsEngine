6e29fcc622c9b13ec18a2089d658f33df22b53bb
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-04-01 13:37:48 +0400

    ray.shape / #int

--- includes/rayintersections2D.h
+++ includes/rayintersections2D.h
@@ -10,5 +10,5 @@ extern "C"
 
 	int IntersectRayCircle(InterRay2DParam* ray, InterCircleParam* circle, float* t, vec2* q);
 	int IntersectRaySquare(InterRay2DParam* ray, InterSquareParam* square, float* tmin, float* tmax, vec2* q);
-
+	int IntersectRayShape(InterRay2DParam* ray, void* obj);
 }
