c966277faecb6cacc7a919d87f20301925136012
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-04-01 13:41:04 +0400

    interesect init segmentcylinder / #int

--- includes/rayintersections3D.h
+++ includes/rayintersections3D.h
@@ -9,4 +9,7 @@ extern "C"
 #endif
 
 	int Intersect3DRaySphere(InterRay3DParam* ray, vec3 ObjP, float* t, vec3* q);
+
+	int Intersect3DSegmentCylinder(InterRay3DParam* ray, vec3 p, vec3 q, float r, float* t, vec3 q_res);
+	
 }
