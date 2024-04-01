b46e2a8b3f3fa60be535290870542688fbc7bd15
KorsarOfficial <bebrusponuhanus@gmail.com>
2024-04-01 13:38:04 +0400

    end

--- includes/rayintersections2D.h
+++ includes/rayintersections2D.h
@@ -11,4 +11,9 @@ extern "C"
 	int IntersectRayCircle(InterRay2DParam* ray, InterCircleParam* circle, float* t, vec2* q);
 	int IntersectRaySquare(InterRay2DParam* ray, InterSquareParam* square, float* tmin, float* tmax, vec2* q);
 	int IntersectRayShape(InterRay2DParam* ray, void* obj);
+
+#ifdef __cplusplus
 }
+#endif
+
+#endif
