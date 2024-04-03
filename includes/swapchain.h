a4fb19d9a4d9cb76e2ff761c86474e00a0e9cd49
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2024-04-03 04:53:49 +0400

    EDS init aft swapPresent

--- includes/swapchain.h
+++ includes/swapchain.h
@@ -15,5 +15,9 @@ extern "C"
 #define MIN(a,b) (((a)<)b))?(a):(b))
 #define MAX(a,b) (((a)<)b))?(a):(b))
 
-	
+	void querySwapChainSupport(void* device, querySwapChainSupport* details);
+
+	EDSurfaceFormatKHR chooseSwapSurfaceFormat(const EDSurfaceFormatKHR* availableFormats, uint32_t sizeFormats);
+
+
 }
