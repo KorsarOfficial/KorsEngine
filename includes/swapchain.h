2c0c771424b9cc608bda980e579b5bc5dc8202d4
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2024-04-03 04:56:05 +0400

    #end

--- includes/swapchain.h
+++ includes/swapchain.h
@@ -22,4 +22,14 @@ extern "C"
 	uint32_t chooseSwapPresentMode(const void** availablePresentModes, uint32_t sizeModes);
 
 	EIExtent2D chooseSwapExtent(const EISurfaceCapabilitiesKHR capabilities);
+
+	void SwapChainCreate();
+	
+
+	void SwapChainCreateImageViews();
+
+#ifdef __cplusplus
 }
+#endif
+
+#endif // SWAPCHAIN_H
