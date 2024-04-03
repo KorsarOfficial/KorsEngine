8e4b5730e3b66239ec6b9aa2f5b4fa2fd97acce8
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2024-04-03 05:42:09 +0400

    uint32_t SBCV init

---
+++ src/shader_builder.c
@@ -0,0 +1,11 @@
+#include "shader_builder.h"
+
+#include "spirv.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+uint32_t ShaderBuilderCheckVariable(ShaderBuilder* builder, ShaderVariableType variable_type, uint32_t* vals, uint32_t size)
+{
+
+}
