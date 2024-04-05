8bf31c93ea9541d23ae1fe0a99873b461e4d3fcb
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2024-04-05 12:55:20 +0400

    AFT SBCV . GOTO  SBFV

--- src/shader_builder.c
+++ src/shader_builder.c
@@ -5,7 +5,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-uint32_t ShaderBuilderCheckVariable(ShaderBuilder* builder, ShaderVariableType variable_type, uint32_t* vals, uint32_t size)
-{
+uint32_t ShaderBuilderCheckVariable(ShaderBuilder* builder, ShaderVariableType variable_type, uint32_t* vals, uint32_t size) {
 
-}
+    uint32_t find = 0;
+    for (int i = 0; i < builder->num_variables; i++)
+    {
+        find = 1;
+
+        if (builder->variables[i].type == variable_type) {
+            for (int j = 0; j < size; j++) {
+                if (builder->variables[i].values[j] != vals[j]) {
+                    find = 0;
+                    break;
+                }
+            }
+
+            if (find)
+                return builder->variables[i].indx;
+        }
+    }
