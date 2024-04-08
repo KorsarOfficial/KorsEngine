0ceaedecd2f50b561f230d80f55edc049f8f9c20
Korsar Official <110203126+KorsarOfficial@users.noreply.github.com>
2024-04-08 23:04:19 +0400

    uint32/ #if [strlen / memcpy

--- src/shader_builder.c
+++ src/shader_builder.c
@@ -229,5 +229,7 @@ uint32_t ShaderBuilderAddArray(ShaderBuilder* builder, ShaderVariableType var_ty
 
     if (name != NULL) {
         uint32_t len = strlen(name);
+
+        memcpy(builder->infos[builder->num_debug_infos].name, name, len);
     }
 }
