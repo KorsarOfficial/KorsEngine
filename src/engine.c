b922283cc95909e785ff03da2d807486d2e9624f
Korsar Official <bebrusponuhanus@gmail.com>
2024-08-05 01:25:56 +0400

    alloc_pipeline_

--- src/engine.c
+++ src/engine.c
@@ -114,4 +114,5 @@ void EngineInitSystem(int width, int height, const char* name){
 
     alloc_buffers_memory_head = calloc(1, sizeof(ChildStack));
     alloc_descriptor_head = calloc(1, sizeof(ChildStack));
+    alloc_pipeline_head = calloc(1, sizeof(ChildStack));
 }
